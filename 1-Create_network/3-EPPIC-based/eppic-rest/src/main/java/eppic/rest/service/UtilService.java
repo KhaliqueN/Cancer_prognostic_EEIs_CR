2bf03f5577287ab1ba33d1678f30ba9e41ec347e
KhaliqueN <78890192+KhaliqueN@users.noreply.github.com>
2024-04-30 14:36:52 +0200

    Add files via upload

---
+++ 1-Create_network/3-EPPIC-based/eppic-rest/src/main/java/eppic/rest/service/UtilService.java
@@ -0,0 +1,64 @@
+package eppic.rest.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import eppic.db.dao.JobDAO;
+import eppic.db.dao.jpa.JobDAOJpa;
+import eppic.rest.commons.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class UtilService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UtilService.class);
+
+    private static final long DISK_SPACE_LEFT_FOR_WARNING = 10000 * 1000; // 10 MB
+
+    public static ObjectNode getInfo() {
+        InetAddress ip;
+        String hostname = "unknown";
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+
+        } catch (UnknownHostException e) {
+            logger.warn("Exception occurred while getting hostname. Will not be able to report host name. Error: {}" , e.getMessage());
+        }
+
+        ObjectNode jsonObj = new ObjectMapper().createObjectNode();
+
+        jsonObj.put("hostname", hostname);
+        jsonObj.put("buildHash", AppConstants.PROJECT_SHA);
+        jsonObj.put("applicationVersion", AppConstants.PROJECT_VERSION);
+        jsonObj.put("apiVersion", AppConstants.MAJOR_VERSION);
+
+        return jsonObj;
+    }
+
+    public static boolean isDbHealthy() {
+        JobDAO jobDAO = new JobDAOJpa();
+        boolean isHealthy = !jobDAO.isJobsEmpty();
+        if (!isHealthy) {
+            logger.error("DB is either unreachable or empty");
+        }
+        return isHealthy;
+    }
+
+    public static boolean isTempDiskHealthy() {
+        File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+
+        long space = tmpDir.getUsableSpace();
+        if (space < DISK_SPACE_LEFT_FOR_WARNING) {
+            logger.error("Java is reporting very low disk usable space in {} dir: only {} bytes available", tmpDir, space);
+            return false;
+        } else {
+            logger.debug("Usable space is {}", space);
+        }
+
+        return true;
+    }
+}
