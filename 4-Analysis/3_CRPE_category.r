226d5741539890c1be69e3240cc8dfe18a7610b9
KhaliqueN <78890192+KhaliqueN@users.noreply.github.com>
2024-05-02 20:14:33 +0200

    Update 3_CRPE_category.r

--- 4-Analysis/3_CRPE_category.r
+++ 4-Analysis/3_CRPE_category.r
@@ -44,93 +44,88 @@ for(qq in 1:length(allnets)){
 
 	in_dir <- paste0('../data/Final_weighted_networks/level1/',strsplit(basename(allnets[qq]),'[.]')[[1]][1])
 	net_file <- data.table::fread(allnets[qq], header=FALSE)
-    perturbed_graph <- list()
+    	perturbed_graph <- list()
 
 	for(gg in 1:length(cancer_type)){
 		c_type <- cancer_type[gg]
-        indir <- paste0(in_dir,'/threshold_',cpm_threshold)
-        gained <- data.table::fread(paste0(indir,'/',c_type,'_gained.txt'))
-        lost <- data.table::fread(paste0(indir,'/',c_type,'_lost.txt'))
-        ##-- a) Non-perturbed edges --------------
-        nonp1 <- gained[gained$patient == 0, ]
-        nonp1 <- igraph::graph_from_data_frame(nonp1[,c(1,2)], directed=FALSE)
-        nonp2 <- lost[lost$patient == 0, ]
-        nonp2 <- igraph::graph_from_data_frame(nonp2[,c(1,2)], directed=FALSE)
-        nonp <- igraph::intersection(nonp1, nonp2)
-        ##-- e) f) Patient-spcific gained and lost ----
-        ps1 <- gained[gained$patient == 1, ]
-        ps1 <- igraph::graph_from_data_frame(ps1[,c(1,2)], directed=FALSE)
-        lost_ig <- igraph::graph_from_data_frame(lost[lost$patient > 0, ][,c(1,2)], directed=FALSE)
-        ps1 <- igraph::difference(ps1, lost_ig) ## e) patient-specific gain
-        ps2 <- lost[lost$patient == 1, ]
-        ps2 <- igraph::graph_from_data_frame(ps2[,c(1,2)], directed=FALSE)
-        gained_ig <- igraph::graph_from_data_frame(gained[gained$patient > 0, ][,c(1,2)], directed=FALSE)
-        ps2 <- igraph::difference(ps2, gained_ig) ## f) patient-specific loss
-        ##-- b) c) only gained/ only lost
-        og <- gained[gained$patient > 1, ]
-        og <- igraph::graph_from_data_frame(og[,c(1,2)], directed=FALSE)
-        og <- igraph::difference(og, lost_ig) ## b) only gained --. gained in at least 2, but never lost
-		ol <- lost[lost$patient > 1, ]
-        ol <- igraph::graph_from_data_frame(ol[,c(1,2)], directed=FALSE)
-        ol <- igraph::difference(ol, gained_ig)
-        ##-- d) gained/lost
-        gl <- igraph::intersection(lost_ig, gained_ig) ## d) gained/lost edges
-        xx <- igraph::ecount(gl)+igraph::ecount(ol)+igraph::ecount(og)+igraph::ecount(ps1)+igraph::ecount(ps2)+igraph::ecount(nonp)
-        if(xx != length(net_file[[1]])){
-        	cat('Error')
-        	break
-        } 
-        ##--- get survival results ------------
-        gained_surv <- data.table::fread(paste0('../results/Final_survival/level1/',strsplit(basename(allnets[qq]),'[.]')[[1]][1],'/threshold_',cpm_threshold,'/',
-            c_type,'_Gained_Surv_.txt'))
-        gained_surv1 <- gained_surv[gained_surv$pval <= pval_thres, ]
-        # gained_surv1 <- gained_surv1[gained_surv1$frac1 <= pval_thres, ]
-        # gained_surv1 <- gained_surv1[gained_surv1$frac2 <= pval_thres, ]
-        gs <- igraph::graph_from_data_frame(gained_surv1[,c(1,2)], directed=FALSE)
-        gs_og <- igraph::intersection(gs, og) ## only gained
-        gs_ps1 <- igraph::intersection(gs, ps1) ## patient-specific
-        ##-------------------------------------
-        ##--- get survival results ------------
-        lost_surv <- data.table::fread(paste0('../results/Final_survival/level1/',strsplit(basename(allnets[qq]),'[.]')[[1]][1],'/threshold_',cpm_threshold,'/',
-            c_type,'_Lost_Surv_.txt'))
-        lost_surv1 <- lost_surv[lost_surv$pval <= pval_thres, ]
-        # lost_surv1 <- lost_surv1[lost_surv1$frac1 <= pval_thres, ]
-        # lost_surv1 <- lost_surv1[lost_surv1$frac2 <= pval_thres, ]
-        ls <- igraph::graph_from_data_frame(lost_surv1[,c(1,2)], directed=FALSE)
-        ls_ol <- igraph::intersection(ls, ol) ## only lost
-        ls_ps2 <- igraph::intersection(ls, ps2) ## patient-specific
-        ##-------------------------------------
-        gs_ls_u <- igraph::union(ls, gs)
-        gs_gl <- igraph::intersection(gs_ls_u, gl) ## gained/lost
-        ##-- store -------------------------------
-        a <- c(a, igraph::ecount(gs_og)/igraph::ecount(gs_ls_u))
-        c <- c(c, igraph::ecount(gs_gl)/igraph::ecount(gs_ls_u))
-        d <- c(d, igraph::ecount(gs_ps1)/igraph::ecount(gs_ls_u))
-        x <- c(x, igraph::ecount(gs_ls_u))
-        b <- c(b, igraph::ecount(ls_ol)/igraph::ecount(gs_ls_u))
-        e <- c(e, igraph::ecount(ls_ps2)/igraph::ecount(gs_ls_u))
-        y <- c(y, igraph::ecount(gs_ls_u))
-        cancer <- c(cancer, cancer_type[gg])
-        netl <- c(netl, strsplit(basename(allnets[qq]),'[.]')[[1]][1])
-        tempss <- igraph::union(gs_og, gs_gl)
-        tempzz <- igraph::union(tempss, gs_ps1)
-        tempjj <- igraph::union(tempzz, ls_ol)
-        perturbed_graph[[gg]] <- igraph::union(tempjj, ls_ps2)
-
-        mut_var <- c(mut_var, igraph::ecount(gs_gl))
-        mut_strict <- c(mut_strict, igraph::ecount(igraph::union(gs_og, ls_ol)))
+	        indir <- paste0(in_dir,'/threshold_',cpm_threshold)
+	        gained <- data.table::fread(paste0(indir,'/',c_type,'_gained.txt'))
+	        lost <- data.table::fread(paste0(indir,'/',c_type,'_lost.txt'))
+	        ##-- a) Non-perturbed edges --------------
+	        nonp1 <- gained[gained$patient == 0, ]
+	        nonp1 <- igraph::graph_from_data_frame(nonp1[,c(1,2)], directed=FALSE)
+	        nonp2 <- lost[lost$patient == 0, ]
+	        nonp2 <- igraph::graph_from_data_frame(nonp2[,c(1,2)], directed=FALSE)
+	        nonp <- igraph::intersection(nonp1, nonp2)
+	        ##-- e) f) Patient-spcific gained and lost ----
+	        ps1 <- gained[gained$patient == 1, ]
+	        ps1 <- igraph::graph_from_data_frame(ps1[,c(1,2)], directed=FALSE)
+	        lost_ig <- igraph::graph_from_data_frame(lost[lost$patient > 0, ][,c(1,2)], directed=FALSE)
+	        ps1 <- igraph::difference(ps1, lost_ig) ## e) patient-specific gain
+	        ps2 <- lost[lost$patient == 1, ]
+	        ps2 <- igraph::graph_from_data_frame(ps2[,c(1,2)], directed=FALSE)
+	        gained_ig <- igraph::graph_from_data_frame(gained[gained$patient > 0, ][,c(1,2)], directed=FALSE)
+	        ps2 <- igraph::difference(ps2, gained_ig) ## f) patient-specific loss
+	        ##-- b) c) only gained/ only lost
+	        og <- gained[gained$patient > 1, ]
+	        og <- igraph::graph_from_data_frame(og[,c(1,2)], directed=FALSE)
+	        og <- igraph::difference(og, lost_ig) ## b) only gained --. gained in at least 2, but never lost
+			ol <- lost[lost$patient > 1, ]
+	        ol <- igraph::graph_from_data_frame(ol[,c(1,2)], directed=FALSE)
+	        ol <- igraph::difference(ol, gained_ig)
+	        ##-- d) gained/lost
+	        gl <- igraph::intersection(lost_ig, gained_ig) ## d) gained/lost edges
+	        xx <- igraph::ecount(gl)+igraph::ecount(ol)+igraph::ecount(og)+igraph::ecount(ps1)+igraph::ecount(ps2)+igraph::ecount(nonp)
+	        if(xx != length(net_file[[1]])){
+	        	cat('Error')
+	        	break
+	        }	 
+	        ##--- get survival results ------------
+	        gained_surv <- data.table::fread(paste0('../results/Final_survival/level1/',strsplit(basename(allnets[qq]),'[.]')[[1]][1],'/threshold_',cpm_threshold,'/',
+	            c_type,'_Gained_Surv_.txt'))
+	        gained_surv1 <- gained_surv[gained_surv$pval <= pval_thres, ]
+	        # gained_surv1 <- gained_surv1[gained_surv1$frac1 <= pval_thres, ]
+	        # gained_surv1 <- gained_surv1[gained_surv1$frac2 <= pval_thres, ]
+	        gs <- igraph::graph_from_data_frame(gained_surv1[,c(1,2)], directed=FALSE)
+	        gs_og <- igraph::intersection(gs, og) ## only gained
+	        gs_ps1 <- igraph::intersection(gs, ps1) ## patient-specific
+	        ##-------------------------------------
+	        ##--- get survival results ------------
+	        lost_surv <- data.table::fread(paste0('../results/Final_survival/level1/',strsplit(basename(allnets[qq]),'[.]')[[1]][1],'/threshold_',cpm_threshold,'/',
+	            c_type,'_Lost_Surv_.txt'))
+	        lost_surv1 <- lost_surv[lost_surv$pval <= pval_thres, ]
+	        # lost_surv1 <- lost_surv1[lost_surv1$frac1 <= pval_thres, ]
+	        # lost_surv1 <- lost_surv1[lost_surv1$frac2 <= pval_thres, ]
+	        ls <- igraph::graph_from_data_frame(lost_surv1[,c(1,2)], directed=FALSE)
+	        ls_ol <- igraph::intersection(ls, ol) ## only lost
+	        ls_ps2 <- igraph::intersection(ls, ps2) ## patient-specific
+	        ##-------------------------------------
+	        gs_ls_u <- igraph::union(ls, gs)
+	        gs_gl <- igraph::intersection(gs_ls_u, gl) ## gained/lost
+	        ##-- store -------------------------------
+	        a <- c(a, igraph::ecount(gs_og)/igraph::ecount(gs_ls_u))
+	        c <- c(c, igraph::ecount(gs_gl)/igraph::ecount(gs_ls_u))
+	        d <- c(d, igraph::ecount(gs_ps1)/igraph::ecount(gs_ls_u))
+	        x <- c(x, igraph::ecount(gs_ls_u))
+	        b <- c(b, igraph::ecount(ls_ol)/igraph::ecount(gs_ls_u))
+	        e <- c(e, igraph::ecount(ls_ps2)/igraph::ecount(gs_ls_u))
+	        y <- c(y, igraph::ecount(gs_ls_u))
+	        cancer <- c(cancer, cancer_type[gg])
+	        netl <- c(netl, strsplit(basename(allnets[qq]),'[.]')[[1]][1])
+	        tempss <- igraph::union(gs_og, gs_gl)
+	        tempzz <- igraph::union(tempss, gs_ps1)
+	        tempjj <- igraph::union(tempzz, ls_ol)
+	        perturbed_graph[[gg]] <- igraph::union(tempjj, ls_ps2)
+	
+	        mut_var <- c(mut_var, igraph::ecount(gs_gl))
+	        mut_strict <- c(mut_strict, igraph::ecount(igraph::union(gs_og, ls_ol)))
 
 	}
 
-    all_pert <- perturbed_graph[[1]]
-    for(cn in 2:length(cancer_type)){
-        all_pert <- igraph::union(all_pert, perturbed_graph[[cn]])
-    }
-
-    # print(igraph::ecount(all_pert))
-
-    # print(igraph::ecount(all_pert)/length(net_file[[1]]))
-
+	all_pert <- perturbed_graph[[1]]
+	for(cn in 2:length(cancer_type)){
+		all_pert <- igraph::union(all_pert, perturbed_graph[[cn]])
+	}
 
 }
 
@@ -162,89 +157,4 @@ for(k in 1:length(lbls)){
 
 }
 
-
-# ##--- correlation plots ----------------
-
-# # pdatax <- data.frame(gl=mut_var, st=mut_strict, Cancer=cancer, Network=netl)
-# pdatax <- data.frame(gl=c*100, st=(a+b)*100, Cancer=cancer, Network=netl)
-
-# for(k in 1:length(lbls)){
-
-#     pdata <- pdatax[pdatax$Network==lbls[k], ]
-#     pdata <- pdata[order(pdata$Cancer), ]
-
-#     ### ---------------------- correlation with known number of mutations ------------------
-#     mutations <- data.table::fread('../data/mutations.csv')
-#     mutations <- mutations[order(mutations$V1), ]
-#     pdata$mutations <- mutations$V2
-
-#     # coral <- cor.test(x=pdata$count, y=pdata$mutations, method = 'spearman')
-#     p <- ggplot(pdata, aes(mutations, gl, color=Cancer)) + 
-#     geom_point(size=2)+
-#     theme(legend.text=element_text(size=12))
-#     basesize <- 12
-#     p <- p + theme_bw(base_size = basesize * 0.8) +
-#     scale_x_continuous(name="Median # of somatic mutations \n per-million base", limits=c(0,max(pdata$mutations))) + 
-#     scale_y_continuous(name="% of CRPEs in the \ngained or lost category", limits=c(0,max(pdata$gl))) +
-#     scale_color_manual(values=c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#ffff99','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#e31a1c','#b15928','black','#9e0142','#053061'))+
-#     theme(axis.text.x = element_text(size = basesize * 0.6, angle = 60, hjust = 0.5,vjust=0.5, colour = "black"),
-#     axis.text.y = element_text(size = basesize * 0.6, angle = 0, hjust = 0.5,vjust=0.5, colour = "black"), 
-#     strip.text = element_text(size = basesize * 0.8), axis.title=element_text(basesize * 0.8))+
-#     guides(color=guide_legend(title="Cancer type",ncol=2))
-#     ggsave(p,filename=paste0("../results/Final_results/Mutations_",net_type[qq],"_gl.png"),width=4.5, height=3, dpi=400)
-
-#     p <- ggplot(pdata, aes(mutations, st, color=Cancer)) + 
-#     geom_point(size=2)+
-#     theme(legend.text=element_text(size=12))
-#     basesize <- 12
-#     p <- p + theme_bw(base_size = basesize * 0.8) +
-#     scale_x_continuous(name="Median # of somatic mutations \n per-million base", limits=c(0,max(pdata$mutations))) + 
-#     scale_y_continuous(name="% of CRPEs in the strictly gained + \nstrictly lost categories", limits=c(0,max(pdata$st))) +
-#     scale_color_manual(values=c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#ffff99','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#e31a1c','#b15928','black','#9e0142','#053061'))+
-#     theme(axis.text.x = element_text(size = basesize * 0.6, angle = 60, hjust = 0.5,vjust=0.5, colour = "black"),
-#     axis.text.y = element_text(size = basesize * 0.6, angle = 0, hjust = 0.5,vjust=0.5, colour = "black"), 
-#     strip.text = element_text(size = basesize * 0.8), axis.title=element_text(basesize * 0.8))+
-#     guides(color=guide_legend(title="Cancer type",ncol=2))
-#     ggsave(p,filename=paste0("../results/Final_results/Mutations_",net_type[qq],"_strict.png"),width=4.5, height=3, dpi=400)
-#     ###----------------------------------------------------------------
-
-#     ### ---------------------- correlation with known degree of heterogeneity ------------------
-#     mutations <- data.table::fread('../data/heterogeniety.csv')
-#     mutations <- mutations[order(mutations$V1), ]
-#     pdata$mutations <- mutations$V2
-
-#     # coral <- cor.test(x=pdata$count, y=pdata$mutations, method = 'spearman')
-#     p <- ggplot(pdata, aes(mutations, gl, color=Cancer)) + 
-#     geom_point(size=2)+
-#     theme(legend.text=element_text(size=12))
-#     basesize <- 12
-#     p <- p + theme_bw(base_size = basesize * 0.8) +
-#     scale_x_continuous(name="Median # of clones", limits=c(0,max(pdata$mutations))) + 
-#     scale_y_continuous(name="% of CRPEs in the \ngained or lost category", limits=c(0,max(pdata$gl))) +
-#     scale_color_manual(values=c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#ffff99','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#e31a1c','#b15928','black','#9e0142','#053061'))+
-#     theme(axis.text.x = element_text(size = basesize * 0.6, angle = 60, hjust = 0.5,vjust=0.5, colour = "black"),
-#     axis.text.y = element_text(size = basesize * 0.6, angle = 0, hjust = 0.5,vjust=0.5, colour = "black"), 
-#     strip.text = element_text(size = basesize * 0.8), axis.title=element_text(basesize * 0.8))+
-#     guides(color=guide_legend(title="Cancer type",ncol=2))
-#     ggsave(p,filename=paste0("../results/Final_results/Heterogeneity_",net_type[qq],"_gl.png"),width=4.5, height=3, dpi=400)
-
-#      # coral <- cor.test(x=pdata$count, y=pdata$mutations, method = 'spearman')
-#     p <- ggplot(pdata, aes(mutations, st, color=Cancer)) + 
-#     geom_point(size=2)+
-#     theme(legend.text=element_text(size=12))
-#     basesize <- 12
-#     p <- p + theme_bw(base_size = basesize * 0.8) +
-#     scale_x_continuous(name="Median # of clones", limits=c(0,max(pdata$mutations))) + 
-#     scale_y_continuous(name="% of CRPEs in the strictly gained + \nstrictly lost categories", limits=c(0,max(pdata$st))) +
-#     scale_color_manual(values=c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#ffff99','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#e31a1c','#b15928','black','#9e0142','#053061'))+
-#     theme(axis.text.x = element_text(size = basesize * 0.6, angle = 60, hjust = 0.5,vjust=0.5, colour = "black"),
-#     axis.text.y = element_text(size = basesize * 0.6, angle = 0, hjust = 0.5,vjust=0.5, colour = "black"), 
-#     strip.text = element_text(size = basesize * 0.8), axis.title=element_text(basesize * 0.8))+
-#     guides(color=guide_legend(title="Cancer type",ncol=2))
-#     ggsave(p,filename=paste0("../results/Final_results/Heterogeneity_",net_type[qq],"_strict.png"),width=4.5, height=3, dpi=400)
-
-
-#     ###----------------------------------------------------------------
-
-# }
 
