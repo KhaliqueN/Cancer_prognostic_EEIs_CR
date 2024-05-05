c592f1a476f81df1b69ef991f13bb880979f48b0
KhaliqueN <78890192+KhaliqueN@users.noreply.github.com>
2024-05-05 16:44:32 +0200

    Delete 1-Create_network directory

--- 1-Create_network/1-contact-based/preprocess_exons.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-# select only exons
-awk -F '\t' '$3 == "exon"' ../data/Homo_sapiens.GRCh38.105.gtf > ../data/processed_exons.tmp
-awk -F '\t' '$3 == "CDS"' ../data/Homo_sapiens.GRCh38.105.gtf > ../data/processed_cds.tmp
-
